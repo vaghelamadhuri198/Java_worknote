0a0ad48b74869b386415bdb11398e655f41c638c
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-03-27 16:48:24 +0530

    Do while.java
    i = 100
    i <= 10  →  100 <= 10  →  false ❌
    i++ → not executed
    System.out.println("hello") → not executed
    
    The loop condition fails immediately.
    No iterations occur.
    
    👉 Therefore, time complexity is:
    
    O(1)  (Constant Time) ✅

---
+++ Do while.java
@@ -0,0 +1,10 @@
+
+public class Condition {
+        public static void main(String[]args) {
+                int i = 100;
+                while (i <= 10) {
+                        i++;
+                        System.out.println("hello");
+                }
+        }
+}
