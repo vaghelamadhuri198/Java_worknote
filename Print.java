afd45297dc6cabdc2b8452c4356deaddfcac13d0
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-03-27 14:05:35 +0530

    Print.java
    System.out.print() is a method used to display output on the console without moving the cursor to the next line after printing.
    
    System.out.println() is a method used to display output on the console and automatically move the cursor to the next line after printing.
    
    Time Complexity Analysis
    1st print → constant time
    2nd print → constant time
    3rd print → constant time
    0(1) total time complexity

---
+++ Print.java
@@ -0,0 +1,11 @@
+public class print {
+        public static void main(String[]args) {
+                //print
+                System.out.print("hello");  
+                
+                System.out.println("madhuri");  
+                System.out.println("vaghela");   
+
+           }
+}
+
