69ffc13a7350d27c75c9f37855afd30fbbe1efe0
Vaghela Madhuri <vaghelamadhuri198@gmail.com>
2026-04-11 11:58:52 +0530

    logical operater.java
    3) Logical Operators 🤔
    Used when combining conditions.
    Operator
    Meaning
    &&
    AND
    `
    
    !
    NOT

---
+++ logical operater.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+
+		System.out.println(a && b); // false
+		System.out.println(a || b); // true
+		System.out.println(!a);     // false
+	}
+}
